618ca01d5a926049c4cde2949b17c5ebe7bdc62c
余友中 <qqlong2323@gmail.com>
2023-05-30 11:14:57 +0800

    modify

--- JavaScirpt/JQWeb03/my.js
+++ JavaScirpt/JQWeb03/my.js
@@ -1,16 +1,34 @@
 $(function () { 
     
+    /*顯示/隱藏*/
+    /*
     $('#AAA').click(function () { 
-        $('img').show();
+        $('img').show(500);
     })
 
     $('#BBB').click(function () {
-        $('img').hide();
+        $('img').hide(500);
     })
 
     $('#CCC').click(function () {
-        $('img').toggle();
+        $('img').toggle(500);
     })
+    */
+
+    /*向下滑動顯示/向上滑動隱藏*/
+    $('#AAA').click(function () {
+        $('img').slideDown();
+    })
+
+    $('#BBB').click(function () {
+        $('img').slideUp();
+    })
+
+    $('#CCC').click(function () {
+        $('img').slideToggle(1500);
+    })
+
+    
 
 
 
