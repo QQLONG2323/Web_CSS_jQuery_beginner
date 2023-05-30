444bd32d5f94fa77d7dcb13b77e878bc4f0cc93d
余友中 <qqlong2323@gmail.com>
2023-05-30 15:46:30 +0800

    modify

--- JavaScirpt/JQWeb08/myJS.js
+++ JavaScirpt/JQWeb08/myJS.js
@@ -1,12 +1,14 @@
 $(function () { 
 
-    $('#BBB').click(function () { 
+    /*固定大小字*/
+
+    /*$('#BBB').click(function () {
         $('#CON_BOX').css('color', '#00f').css('font-size', '30px')        
-    })
+    })*/
 
     /*上下兩種CSS的方式都可用，看自己用哪個順手*/
 
-    $('#MMM').click(function () {
+    /*$('#MMM').click(function () {
         $('#CON_BOX').css({
             'color': '',
             'font-size': '22px',
@@ -18,7 +20,29 @@ $(function () {
             'color': 'green',
             'font-size': '14px',
             'text-align': 'center',
-        })
+        })*/
+
+    /*==========*/
+
+    /*每次按都能增加或減少*/
+    
+    var N = 16;
+
+    $('#BBB').click(function () {
+        N = N + 2;
+
+        $('#CON_BOX').css('color', '#00f').css('font-size', N+'px')        
+    })
+    
+    $('#SSS').click(function () {
+        N = N - 2;
+
+        $('#CON_BOX').css({
+            'color': '',
+            'font-size': N+'px',
+            'text-align': 'center',
+        }) 
+
     })
 
 
