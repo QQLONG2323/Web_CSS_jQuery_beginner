472bdee4aad896c531ddbadb9c40342687b9bfba
余友中 <qqlong2323@gmail.com>
2023-05-30 14:51:39 +0800

    modify

--- JavaScirpt/JQWeb08/myJS.js
+++ JavaScirpt/JQWeb08/myJS.js
@@ -4,11 +4,21 @@ $(function () {
         $('#CON_BOX').css('color', '#00f').css('font-size', '30px')        
     })
 
+    /*上下兩種CSS的方式都可用，看自己用哪個順手*/
+
     $('#MMM').click(function () {
         $('#CON_BOX').css({
-            'color': '#00f',
-            'font-size': '18px',
+            'color': '',
+            'font-size': '22px',
             'text-align':'center',})
+    })
+
+    $('#SSS').click(function () {
+        $('#CON_BOX').css({
+            'color': 'green',
+            'font-size': '14px',
+            'text-align': 'center',
+        })
     })
 
 
