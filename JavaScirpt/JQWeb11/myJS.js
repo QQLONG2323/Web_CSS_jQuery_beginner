4cbf538d2570303770816a9ee831dabf5da235bf
余友中 <qqlong2323@gmail.com>
2023-05-30 16:44:02 +0800

    modify

--- JavaScirpt/JQWeb11/myJS.js
+++ JavaScirpt/JQWeb11/myJS.js
@@ -1,1 +1,21 @@
+$(function () { 
 
+    /*stop(是否取消連續觸發, 是否馬上結束「前面」的動畫)*/
+
+    $('#RRR').click(function () {
+        $("#KIDS").stop(true,false).animate({ left: 1000 }, 600);
+    });
+
+    $('#LLL').click(function () {
+        $("#KIDS").stop(true, false).animate({ left: 0 }, 5000);
+    });
+
+
+
+
+
+
+
+
+
+})
