c50eaf714fdae1ecd9bb333cc510b497f9cfafdd
余友中 <qqlong2323@gmail.com>
2023-05-30 10:54:18 +0800

    modify

---
+++ JavaScirpt/JQWeb03/my.js
@@ -0,0 +1,19 @@
+$(function () { 
+    
+    $('#AAA').click(function () { 
+        $('img').show();
+    })
+
+    $('#BBB').click(function () {
+        $('img').hide();
+    })
+
+    $('#CCC').click(function () {
+        $('img').toggle();
+    })
+
+
+
+
+
+})
