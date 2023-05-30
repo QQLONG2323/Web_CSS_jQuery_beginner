ee8879bbbe570c9fab62abc3b2ac3fe3ff87513f
余友中 <qqlong2323@gmail.com>
2023-05-30 14:43:17 +0800

    modify

---
+++ JavaScirpt/JQWeb08/myJS.js
@@ -0,0 +1,37 @@
+$(function () { 
+
+    $('#BBB').click(function () { 
+        $('#CON_BOX').css('color', '#00f').css('font-size', '30px')        
+    })
+
+    $('#MMM').click(function () {
+        $('#CON_BOX').css({
+            'color': '#00f',
+            'font-size': '18px',
+            'text-align':'center',})
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
