0b2c75e0b1790365c11c3a2b37957ab51f795603
余友中 <qqlong2323@gmail.com>
2023-05-30 14:12:27 +0800

    modify

--- JavaScirpt/JQWeb07/myJS.js
+++ JavaScirpt/JQWeb07/myJS.js
@@ -5,12 +5,35 @@ $(function () {
     },
         function () {
             $('#SUB1').stop(true, true).slideUp(500);
-        }
-
-
-
-
-    )
+        })
+    
+    $('#MENU2').hover(function () {
+        $('#SUB2').stop(true, true).slideDown(500);
+    },
+        function () {
+            $('#SUB2').stop(true, true).slideUp(500);
+        })
+    
+    $('#MENU3').hover(function () {
+        $('#SUB3').stop(true, true).slideDown(500);
+    },
+        function () {
+            $('#SUB3').stop(true, true).slideUp(500);
+        })
+    
+    $('#MENU4').hover(function () {
+        $('#SUB4').stop(true, true).slideDown(500);
+    },
+        function () {
+            $('#SUB4').stop(true, true).slideUp(500);
+        })
+    
+    $('#MENU5').hover(function () {
+        $('#SUB5').stop(true, true).slideDown(500);
+    },
+        function () {
+            $('#SUB5').stop(true, true).slideUp(500);
+        })
 
 
 
