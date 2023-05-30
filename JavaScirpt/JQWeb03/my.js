a4a0f3893fde9e51bcf990d697d371e2c50488da
余友中 <qqlong2323@gmail.com>
2023-05-30 11:24:07 +0800

    modify

--- JavaScirpt/JQWeb03/my.js
+++ JavaScirpt/JQWeb03/my.js
@@ -16,6 +16,7 @@ $(function () {
     */
 
     /*向下滑動顯示/向上滑動隱藏*/
+    /*
     $('#AAA').click(function () {
         $('img').slideDown();
     })
@@ -27,11 +28,34 @@ $(function () {
     $('#CCC').click(function () {
         $('img').slideToggle(1500);
     })
-
+    */
     
+    /*淡入/淡出*/
+    /*
+    $('#AAA').click(function () {
+        $('img').fadeIn();
+    })
 
+    $('#BBB').click(function () {
+        $('img').fadeOut();
+    })
 
+    $('#CCC').click(function () {
+        $('img').fadeToggle();
+    })
+    */
+    
+    /*直接調整透明度*/
+    $('#AAA').click(function () {
+        $('img').fadeTo(500, 1);
+    })
 
+    $('#BBB').click(function () {
+        $('img').fadeTo(500, 0);
+    })
 
+    $('#CCC').click(function () {
+        $('img').fadeTo(500, 0.5);
+    })
 
 })
