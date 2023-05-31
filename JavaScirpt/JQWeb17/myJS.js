225780f5e56f65f362b84d5c43847b72da1cd6d2
余友中 <qqlong2323@gmail.com>
2023-05-31 11:57:25 +0800

    modify

--- JavaScirpt/JQWeb17/myJS.js
+++ JavaScirpt/JQWeb17/myJS.js
@@ -1,2 +1,51 @@
+$(function () { 
 
+    $(window).scroll(function () { 
+        var hh = $(window).scrollTop() + 150;
+        $('#MENU').stop(true, true).animate({ top: hh }, 600);
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
  
