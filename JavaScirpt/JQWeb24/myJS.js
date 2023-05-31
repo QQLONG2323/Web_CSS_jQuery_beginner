72a14ec1ce9fc31bacfb118a994113dc24af959b
余友中 <qqlong2323@gmail.com>
2023-05-31 16:39:05 +0800

    modify

---
+++ JavaScirpt/JQWeb24/myJS.js
@@ -0,0 +1,36 @@
+$(function () { 
+
+    $('.BANNER').click(function () { 
+
+        $(this).find('.BOX').slideDown().end().siblings().find('.BOX').slideUp()
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
