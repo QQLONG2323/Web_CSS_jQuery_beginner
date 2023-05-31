28782d64032facd5557427b45650f2edb5e168de
余友中 <qqlong2323@gmail.com>
2023-05-31 16:27:12 +0800

    modify

---
+++ JavaScirpt/JQWeb23/myJS.js
@@ -0,0 +1,33 @@
+$(function(){
+
+    $('.small img').click(function () {
+        
+        var N = $(this).index();
+
+        $('.big img').eq(N).fadeIn().siblings().fadeOut()
+
+
+
+    })
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
