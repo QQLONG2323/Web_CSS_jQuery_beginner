0bb9da94378d838807d1002cf45b4410ea8f2886
余友中 <qqlong2323@gmail.com>
2023-06-01 11:50:40 +0800

    modify

---
+++ JavaScirpt/JQWeb13/myJS.js
@@ -0,0 +1,40 @@
+$(function () { 
+
+    $('#menu').hover(function () {
+        
+        $(this).stop(true, false).animate({ left: 0 }, 600, 'easeOutBounce');
+
+
+
+    }, function () {
+
+        $(this).stop(true, false).animate({ left: -158 }, 600, 'easeOutBounce');
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
