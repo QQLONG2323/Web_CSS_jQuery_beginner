071bac27862b79b760b08ca3acaba5506838509d
余友中 <qqlong2323@gmail.com>
2023-06-01 14:44:48 +0800

    modify

---
+++ JavaScirpt/JQWeb15/myJS.js
@@ -0,0 +1,27 @@
+$(function () { 
+
+    $('#gotop').click(function () { 
+
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        event.preventDefault();
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
